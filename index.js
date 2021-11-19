8eb3841a2ec6897fcdbac0822bd76c8cf6143c4c
JazzPants <leighton.l14@gmail.com>
2021-11-19 18:21:37 +1100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+// function superbowlWin(array, key, value) {
+//     array.find(function(value) {
+//         return value === 'W'
+//     }) 
+//     if (x === 'W') {
+//         return array.find (function(s) {return s === "W"})
+//     }
+// }
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+function superbowlWin(x) {
+    let result = x.find( function(x) { 
+        return x.result === 'W' //find an element with W, assign it 'x'
+    })
+    if (result) { //if result finds a winning date
+        return result.year
+    } else { //if result doesn't find a winning date
+        return undefined
+    }
+}
+
+console.log(superbowlWin(record));
